27f1674500825e57849d85b64ac979358d90f392
mariodsouza1999 <103345859+mariodsouza1999@users.noreply.github.com>
2022-08-14 15:37:50 -0400

    Environment  variable using ConfigMap

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ DB_Database = os.environ.get('DB_Database') or "mysql"
 DB_User = os.environ.get('DB_User') or "root"
 DB_Password = os.environ.get('DB_Password') or "paswrd"
 bg_image = os.environ.get('bg_image') or "no image"
+NAME =  os.environ.get('NAME')
 
 @app.route("/")
 def main():
